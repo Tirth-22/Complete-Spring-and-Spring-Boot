378a26ee3e3befa1ce83057fd49ebfbd03aee87c
Tirth-22 <tirthmakadia2006@gmail.com>
2025-12-05 11:41:39 +0530

    first-commit

---
+++ spring-framwork-01/src/test/java/com/in28min/spring_framwork/SpringFramworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28min.spring_framwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFramworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
