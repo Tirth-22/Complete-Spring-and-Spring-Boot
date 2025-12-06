e9b012179a61f29b0a057fb2fc92ba0c8d903c37
Tirth Makadia <tirthmakadia2006@gmail.com>
2025-12-06 11:55:21 +0530

    springboot restcontroller & request mapping

---
+++ 03-spring-boot/src/main/java/com/tirth/spring_boot/Course.java
@@ -0,0 +1,36 @@
+package com.tirth.spring_boot;
+
+public class Course {
+
+    private long id;
+    private String name;
+    private String author;
+
+    public Course(long id, String name, String author) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
