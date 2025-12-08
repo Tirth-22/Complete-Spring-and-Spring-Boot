a1af0942f805b4270329adbbade2075d3434535a
Tirth-22 <tirthmakadia2006@gmail.com>
2025-12-08 22:07:25 +0530

    spring core

---
+++ 02-spring-framwork/src/main/java/com/in28min/spring_framwork/examples/c1/DataService.java
@@ -0,0 +1,5 @@
+package com.in28min.spring_framwork.examples.c1;
+
+public interface DataService {
+    int[] retrieveData();
+}
---
+++ 02-spring-framwork/src/main/java/com/in28min/spring_framwork/examples/f1/PrePostAnnotationLauncherApplication.java
@@ -0,0 +1,61 @@
+package com.in28min.spring_framwork.examples.f1;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+class SomeClass {
+
+    private SomeDependency someDependency;
+
+    public SomeClass(SomeDependency someDependency) {
+        super();
+        this.someDependency = someDependency;
+        System.out.println("All Dependencies are ready");
+    }
+
+    @PostConstruct
+    public void intialize() {
+        someDependency.getReady();
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("All Dependencies Destroyed");
+    }
+}
+
+@Component
+class SomeDependency{
+
+    public void getReady() {
+        System.out.println("Some Dependency Ready");
+    }
+}
+
+@Configuration
+@ComponentScan
+class ClassA
+{}
+@Configuration
+@ComponentScan
+public class PrePostAnnotationLauncherApplication {
+
+    public static void main(String[] args) {
+
+        try (var context =
+                     new AnnotationConfigApplicationContext
+                             (PrePostAnnotationLauncherApplication.class)) {
+
+            Arrays.stream(context.getBeanDefinitionNames())
+                    .forEach(System.out::println);
+
+        }
+    }
+}
