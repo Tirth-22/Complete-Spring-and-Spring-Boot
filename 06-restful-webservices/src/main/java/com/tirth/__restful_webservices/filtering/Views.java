8227882d2db8e99873752061c65460dd8fad8e12
Tirth-22 <tirthmakadia2006@gmail.com>
2025-12-20 13:18:49 +0530

    filtering

---
+++ 06-restful-webservices/src/main/java/com/tirth/__restful_webservices/filtering/Views.java
@@ -0,0 +1,11 @@
+package com.tirth.__restful_webservices.filtering;
+
+public class Views {
+
+    public interface Public{
+
+    }
+
+    public interface Internal extends Public{
+    }
+}
---
+++ 06-restful-webservices/src/main/java/com/tirth/__restful_webservices/versioning/VersioningPersonController.java
@@ -0,0 +1,4 @@
+package com.tirth.__restful_webservices.versioning;
+
+public class VersioningPersonController {
+}
