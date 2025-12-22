b7a89aca504e27581cfb7eec6b173368c986d4b6
Tirth-22 <tirthmakadia2006@gmail.com>
2025-12-22 22:11:22 +0530

    repository layer talk to mysql using spring data jpa

--- 06-restful-webservices/src/main/java/com/tirth/__restful_webservices/jpa/PostRepository.java
+++ 06-restful-webservices/src/main/java/com/tirth/__restful_webservices/jpa/PostRepository.java
@@ -1,4 +1,7 @@
 package com.tirth.__restful_webservices.jpa;
 
-public interface PostRepository {
+import com.tirth.__restful_webservices.user.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post,Integer> {
 }
--- 06-restful-webservices/src/main/java/com/tirth/__restful_webservices/jpa/UserRepository.java
+++ 06-restful-webservices/src/main/java/com/tirth/__restful_webservices/jpa/UserRepository.java
@@ -1,5 +1,8 @@
-package com.tirth.__restful_webservices.user;
+package com.tirth.__restful_webservices.jpa;
 
-public class UserRepository {
-    
+import com.tirth.__restful_webservices.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+
 }
