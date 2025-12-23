4c971db3ff8679d6d7649cc7b9472f64a4044547
Tirth-22 <tirthmakadia2006@gmail.com>
2025-12-23 23:54:25 +0530

    components

---
+++ 09-full-stack/frontend/todo-app/src/component/learning-component/FirstComponent.jsx
@@ -0,0 +1,15 @@
+export default function FirstComponent() {
+  return (
+    <div>
+      This is my first component
+    </div>
+  )
+}
+
+export function ThirdComponent() {
+    return (
+        <div>
+            This is my third component
+        </div>
+    )
+}
---
+++ 09-full-stack/frontend/todo-app/src/component/learning-component/SecondComponent.jsx
@@ -0,0 +1,7 @@
+export default function SecondComponent() {
+  return (
+    <div>
+      This is my second component
+    </div>
+  )
+}
