abad78beaa7d10f90f1ca3500b58802499585314
Tirth-22 <tirthmakadia2006@gmail.com>
2026-01-01 18:19:01 +0530

    connect spring api to welcome page axios

--- 09-full-stack/frontend/todo-app/src/components/Welcome.jsx
+++ 09-full-stack/frontend/todo-app/src/components/Welcome.jsx
@@ -1,15 +1,43 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
+import axios from 'axios'
+import { useState } from 'react'
+import { retriveHelloWorldBean, retriveHelloWorldPathvariable } from './api/HelloWorldSerice'
 
 const Welcome = () => {
- const {username} = useParams()
+  const { username } = useParams()
 
+  const [message, setmessage] = useState(null)
+
+  function callHelloWorldApi(){
+
+    retriveHelloWorldPathvariable("tirth")
+      .then((response) => { SuccessfulResponse(response) })
+      .catch((error) => { ErrorResponse(error) })
+      .finally( () => {console.log("cleanup");
+      })
+  }
+
+  function SuccessfulResponse(response) {
+    console.log(response.data.message);
+    setmessage(response.data.message)
+  }
+
+  function ErrorResponse(error) {
+    console.log(error);
+  }
+  
   return (
     <div>
-    <div className='h-screen items-center flex flex-col justify-center'>
-      <h1 className='font-bold text-4xl'>Welcome {username}</h1>
-      <p><Link className="text-blue-500 underline" to={"/todo"}>Manage</Link> your todos here</p>    
-    </div>
+      <div className='h-screen items-center flex flex-col justify-center'>
+        <h1 className='font-bold text-4xl'>Welcome {username}</h1>
+        <p><Link className="text-blue-500 underline" to={"/todo"}>Manage</Link> your todos here</p>
+        <button onClick={callHelloWorldApi} className='cursor-pointer rounded-full py-2 px-4 bg-green-600 text-white my-6'>Call Hello api
+        </button>
+        <p>
+          {message}
+        </p>
+      </div>
     </div>
   )
 }
---
+++ 09-full-stack/frontend/todo-app/src/components/api/HelloWorldSerice.jsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const apiClient = axios.create(
+    {
+        baseURL : 'http://localhost:8080'
+    }
+)
+
+export const retriveHelloWorldBean = () => apiClient.get('/hello-world-bean')
+
+export const retriveHelloWorldPathvariable = (username) => apiClient.get(`/hello-world/path-variable/${username}`)
